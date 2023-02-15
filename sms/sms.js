b54bc510418555084169008c135feeac045aa902
Jill-7span <113359267+Jill-7span@users.noreply.github.com>
2023-02-15 16:35:21 +0530

    dockerize it

--- sms/sms.js
+++ sms/sms.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const smsController = require("./smsController");
 const middleware = require("../middleware/authMiddleware");
 
+router.get("/testSms", smsController.test)
 
 router.post("/sendSms", middleware.authOfBusiness, smsController.sendSms);
 
